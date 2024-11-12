2089ec383e6e6b0094d4f7d85d4fc73fd12eab46
Rohit Singh <rsrohitsingh682@gmail.com>
2024-11-12 19:00:24 +0530

    refactor: use `name` as argument for simplicity

--- default.nix
+++ default.nix
@@ -20,9 +20,9 @@ pog.pog {
       argument = "PREFIX";
     }
     {
-      name = "derivations";
+      name = "name";
       description = "The list of derivations to do whitelisted cachix push and pin";
-      argument = "DERIVATIONS";
+      argument = "NAME";
     }
   ];
   runtimeInputs = [ jq cachix git ];
@@ -30,7 +30,7 @@ pog.pog {
   script = helpers: ''
     blue "Parsing omnix JSON (subflake=$subflake)"
 
-    STORE_PATHS=$(jq -r --arg prefix "$prefix" --arg name "$subflake" --arg derivations "$derivations" -f ${./script.jq})
+    STORE_PATHS=$(jq -r --arg prefix "$prefix" --arg subflake "$subflake" --arg derivations "$name" -f ${./script.jq})
 
     green "Pushing to https://''${cache}.cachix.org"
     echo "$STORE_PATHS" | jq -r --arg cache "$cache" '"cachix push \($cache) \(values | join(" "))"' | sh
